a0bdcd475abd0504dac66d63da1477362a2ab9e4
Bonaventure <bonarhyme@gmail.com>
2021-04-11 23:29:17 +0100

    Completed the assignment

--- mockAtm.py
+++ mockAtm.py
@@ -1,7 +1,7 @@
 import random
 from datetime import datetime 
 
-database = {}
+database = {4363657467: ["bona@gmail.com", "bona", "chukwudi", "1234567", 3567]}
 
 def init():
     print("Welcome to Bank Bona \n \n")
@@ -27,7 +27,12 @@ def login():
             if(accountNumber == accountNumberFromUser):
                 if(userDetails[3] == password):
                     isLoginSuccessful = True
-                    
+                    currentTime = datetime.now().time()
+                    currentDate = datetime.now().strftime('%y-%m-%d')
+                    print("Welcome %s %s!!!" % (userDetails[1], userDetails[2]))
+                    print("Date: %s" %currentDate)
+                    print("Time: %s" %currentTime)
+
                     bankOperation(userDetails)
                 else: 
                     print("Invalid account or password")
@@ -44,7 +49,7 @@ def register():
     # confirmPassword = input("Confirm your password \n")
     accountNumber = generateAccountNumber()
 
-    database[accountNumber] = [email, firstName, lastName, password]
+    database[accountNumber] = [email, firstName, lastName, password, 0]
     
     print("Your account has been created")
     print("**** *********  ***********  ********")
@@ -55,34 +60,49 @@ def register():
     login()
 
 def bankOperation(user):
-    currentTime = datetime.now().time()
-    currentDate = datetime.now().strftime('%y-%m-%d')
-    print("Welcome %s %s!!!" % (user[1], user[2]))
-    print("Date: %s" %currentDate)
-    print("Time: %s" %currentTime)
-
-    selectedOption = int(input("What do you want to do? \n (1) deposit \n (2) withdraw \n (3) complaint \n (4) logout \n (5) exit \n"))
+    selectedOption = int(input("What do you want to do? \n (1) deposit \n (2) withdraw \n (3) balance \n (4) complaint \n (5) logout \n (6) exit \n"))
     
     if(selectedOption == 1):       
-        depositOperation()
+        depositOperation(user)
     elif(selectedOption == 2):
-        withdrawalOperation()      
+        withdrawalOperation(user)      
     elif(selectedOption == 3):
-        complaint()
+        balance(user)      
     elif(selectedOption == 4):
-        logout()
+        complaint()
     elif(selectedOption == 5):
+        logout()
+    elif(selectedOption == 6):
         exit()
     else:
         print("Invalid option selected")
         bankOperation(user)
     
 
-def withdrawalOperation():
-    print("Withdrawal")
+def withdrawalOperation(user):
+    print("Your available balance is %d" % (user[4]))
+    withdrawal = int(input("How much do you want to withdraw? \n"))
 
-def depositOperation():
-    print("Deposit")
+    if(withdrawal < user[4]):
+        print("Your withdrawal of %d was successful" % (withdrawal))
+        dep = user[4] - withdrawal
+        print("Your current balance is %d \n" % (dep))
+
+        bankOperation(user)
+    else: 
+        print("Your balance is so small.... Please make a deposit")
+        bankOperation(user)
+
+def depositOperation(user):
+    deposit = int(input("How much do you want to deposit? \n"))
+
+    if(deposit > 0):
+        print("Your deposit of %d was successful" % (deposit))
+        dep = deposit + user[4]
+        print("Your current balance: %d \n" % (dep))
+        
+        bankOperation(user)
+
 
 def generateAccountNumber():
     return random.randrange(1111111111, 9999999999)
@@ -91,6 +111,12 @@ def complaint():
     complaint = input("What issue will you like to complain about? \n")
     print("Your complaint: %s \n Has been received" %complaint )
     print("Thank you for contacting us.")
+
+def balance(user):
+    print("**** *********  ***********  ********")
+    print("Your current balance is %d" % user[4])
+    print("**** *********  ***********  ********")
+    bankOperation(user)
 
 def logout():
     login()
